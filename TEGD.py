5850b8ea8972ba1977b84876965e38f017aee56e
SofiaW <117039398+SofiShiny@users.noreply.github.com>
2025-04-27 21:39:49 -0400

    mas publicaciones

--- TEGD.py
+++ TEGD.py
@@ -11,7 +11,7 @@ reddit = praw.Reddit(
 subreddit = reddit.subreddit("NintendoSwitch2")
 
 # Filtrar publicaciones recientes y buscar comentarios
-for post in subreddit.new(limit=10):  # Cambia 'new' por 'hot' o 'top' si lo prefieres
+for post in subreddit.new(limit=100):  # Cambia 'new' por 'hot' o 'top' si lo prefieres
     print(f"Título de la publicación: {post.title}")
     
     # Obtener comentarios de la publicación
